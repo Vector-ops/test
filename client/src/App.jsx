d15871fb3503e13b4a847efd02b425dd5a6ebc36
Shivam77607 <shivamsharma77607@gmail.com>
2023-11-14 20:50:05 +0530

    Add response state and update handleSubmit function to handle server response

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,62 +2,62 @@ import axios from "axios";
 import { useState } from "react";
 
 function App() {
-	const [id, setId] = useState("");
-	const [name, setName] = useState("");
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [response, setResponse] = useState("");
 
-	async function handleSubmit(e) {
-		e.preventDefault();
-		// fetch(`${import.meta.env.VITE_APP_SERVER_URL}/login`, {
-		// 	method: "POST",
-		// 	body: {
-		// 		fullName: name,
-		// 		employeeId: id,
-		// 	},
-		// 	headers: {
-		// 		"Content-type": "application/json; charset=UTF-8",
-		// 	},
-		// });
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ firstName: name, employeeId: id });
 
-		const response = await axios.post(
-			`${import.meta.env.VITE_APP_SERVER_URL}/login`,
-			{
-				fullName: name,
-				employeeId: id,
-			}
-		);
+    axios
+      .post(`${import.meta.env.VITE_APP_SERVER_URL}/login`, {
+        fullName: name,
+        employeeId: id,
+      })
+      .then((response) => {
+        setResponse(response);
+        if (!response.ok) {
+          throw new Error(
+            "Network response was not ok: " + response.statusText
+          );
+        }
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }
 
-		console.log(response);
-	}
-
-	return (
-		<div className="box">
-			<h1>Login Test Modal</h1>
-			<form onSubmit={(e) => handleSubmit(e)}>
-				<input
-					type="text"
-					placeholder="Enter id"
-					value={id}
-					onChange={(e) => setId(e.target.value)}
-					required
-				/>
-				<input
-					type="text"
-					placeholder="Enter name"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					required
-				/>
-				<button type="submit">Submit</button>
-			</form>
-			<a
-				href={`${import.meta.env.VITE_APP_CLIENT_URL}?employeeId=${id}`}
-				target="_blank"
-				rel="noreferrer"
-			>
-				Redirect
-			</a>
-		</div>
-	);
+  return (
+    <div className="box">
+      <h1>Login Test Modal</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Enter id"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <a
+        href={`${import.meta.env.VITE_APP_CLIENT_URL}?employeeId=${id}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Redirect
+      </a>
+      <p>{response.data}</p>
+    </div>
+  );
 }
 
 export default App;
